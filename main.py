388088f1a0280af16e70d4d972a33bfcb38de648
IraD <dyakova1293@mail.com>
2019-10-12 09:42:22 +0700

    Добавлена возможность входа в личный аккаунт НГТУ

--- main.py
+++ main.py
@@ -8,31 +8,25 @@ password = nums[1]
 
 driver = webdriver.Chrome(executable_path = 'C:/TEMP/DYAKOVA/web/chromedriver.exe')
 
-driver.get('https://github.com/login')
+driver.get('https://login.nstu.ru/ssoservice/XUI/#login/&goto=http%3A%2F%2Fciu.nstu.ru%2Fstudent_study%2F')
 
-input_login = driver.find_element_by_name('login')
+time.sleep(15)
+
+input_login = driver.find_element_by_name('callback_0')
 input_login.send_keys(login)
 
-input_password = driver.find_element_by_name('password')
+input_password = driver.find_element_by_name('callback_1')
 input_password.send_keys(password)
 
-input_click = driver.find_element_by_name('commit')
+time.sleep(5)
+
+input_click = driver.find_element_by_name('callback_2')
 input_click.click()
----------------------------------------
-import time
-from selenium import webdriver
 
-with open('p1','r') as f:
-    nums = f.read().splitlines()
-login = nums[0]
-password = nums[1]
 
-driver = webdriver.Chrome(executable_path = 'C:/TEMP/DYAKOVA/web/chromedriver.exe')
 
-driver.get('https://www.nstu.ru/')
 
-in_link = driver.find_element_by_css_selector('a.header__login-link')
-in_link.click()
+
 
 
 
